89b12eb431e4ec87eca21acdd6728389d35fb2d9
siddht4 <15411956+siddht4@users.noreply.github.com>
2022-08-28 02:24:04 +0530

    Decryptor,encryptor
    Made code into individual functions

--- decryptor.py
+++ decryptor.py
@@ -1,38 +1,47 @@
+
 from Crypto.Cipher import AES
 import scrypt
 import os
 import binascii
 import secrets
 import pickle
-with open('salt_data.bin','rb') as _bin:
-    _data=pickle.load(_bin)
-def decrypt_AES_GCM(_file):
-   # (kdfSalt, nonce) = encryptedMsg
+def load_salt():
+    with open('salt_data.bin','rb') as _bin:
+        return f_salt(pickle.load(_bin))
+def f_salt(_):
+    secretKey=_['secret_key']
+    nonce=_['aes_cipher']
+    return (secretKey,nonce)
+def _dec_file(_file,secretKey,nonce):
+    print(' Decrypting File -----> '.format(_file))
     with open(_file,'rb') as _bin_r:
         _data_2=pickle.load(_bin_r)
     ciphertext=_data_2['cipher_text']  
     authTag=_data_2['auth_tag']
-    
-    secretKey=_data['secret_key']
-    nonce=_data['aes_cipher']
     aesCipher = AES.new(secretKey, AES.MODE_GCM, nonce)
-    plaintext = aesCipher.decrypt_and_verify(ciphertext,authTag)
+    decrypt_ntext = aesCipher.decrypt_and_verify(ciphertext,authTag)
     with open(_file,'wb') as _bin_w:
-        _bin_w.write(plaintext)
-    return ' Decrypted'
-
-
-_file='_g'
-if os.path.isdir(_file): 
-    print("It is a directory")  
-    print(' doing Fdr Loop')
-    for r, d, f in os.walk(_file):
-        for file in f:
-            _path=r+'/'+file
-            decryptedMsg = decrypt_AES_GCM(_path)
-            print('{}  has Been Decrypted'.format(_path))
-elif os.path.isfile(_file): 
-    decryptedMsg = decrypt_AES_GCM(_file)
+        _bin_w.write(decrypt_ntext)
     print('{}  has Been Decrypted'.format(_file))
-else:
-    print(' special File')
+        
+def _fold(_,s,n):
+    for __ in os.listdir(_):
+        file_T(_+'/'+__,s,n)
+def file_T(_file,s_key,nonce):
+    if os.path.isfile(_file):
+        print(' {}  Is a  File '.format(_file))
+        _dec_file(_file,s_key,nonce)
+    elif os.path.isdir(_file):
+        print(' {} is a directory '.format(_file))
+        _fold(_file,s_key,nonce)
+    else:
+        print(" {} is a special file (socket, FIFO, device file)".format(_file))
+    
+def decrypt_AES_GCM(_file):
+    secretkey,nonce=load_salt()
+    print(secretkey)
+    print(nonce)
+    file_T(_file,secretkey,nonce)
+
+_path='de'
+decrypt_AES_GCM(_path)
--- encryptor.py
+++ encryptor.py
@@ -4,59 +4,44 @@ import os
 import binascii
 import secrets
 import pickle
+def load_salt():
+    with open('salt_data.bin','rb') as _bin:
+        return f_salt(pickle.load(_bin))
+def f_salt(_):
+    secretKey=_['secret_key']
+    nonce=_['aes_cipher']
+    return (secretKey,nonce)
+def _enc_file(_file,secretKey,nonce):
+    print(' encrypting file -----> {}'.format(_file))
+    with open(_file,'rb') as _read:
+        _data=_read.read()
+    _aesCipher = AES.new(secretKey, AES.MODE_GCM, nonce)
+    ciphertext, authTag = _aesCipher.encrypt_and_digest(_data)
+    _d_cip_auth={}
+    _d_cip_auth['cipher_text']=ciphertext
+    _d_cip_auth['auth_tag']=authTag
+    with open(_file,'wb') as _writ_r:
+        pickle.dump(_d_cip_auth,_writ_r)
+    print(' {} . -----> Encrypted'.format(_file))
+def _fold(_,s,n):
+    for __ in os.listdir(_):
+        file_T(_+'/'+__,s,n)
+def file_T(_file,s_key,nonce):
+    if os.path.isfile(_file):
+        print('{}  Is a File'.format(_file))
+        _enc_file(_file,s_key,nonce)
+    elif os.path.isdir(_file):
+        print('{} is  directory'.format(_file))
+        _fold(_file,s_key,nonce)
+    else:
+        print(" {} is a special file (socket, FIFO, device file)".format(_file))
+    
     
 def encrypt_AES_GCM(_file):
-    with open('salt_data.bin','rb') as _bin:
-        _enc_k=pickle.load(_bin)
+    secretkey,nonce=load_salt()
+    print(secretkey)
+    print(nonce)
+    file_T(_file,secretkey,nonce)
 
-    secretKey=_enc_k['secret_key']
-    nonce=_enc_k['aes_cipher']
-    print(secretKey)
-
-    if os.path.isdir(_file): 
-        print("It is a directory")  
-        print(' doing Fdr Loop')
-        for r, d, f in os.walk(_file):
-            for file in f:
-                _path=r+'/'+file
-                with open(_path,'rb') as _read:
-                    _data=_read.read()
-                _aesCipher = AES.new(secretKey, AES.MODE_GCM, nonce)
-                ciphertext, authTag = _aesCipher.encrypt_and_digest(_data)
-                _d_cip_auth={}
-                _d_cip_auth['cipher_text']=ciphertext
-                _d_cip_auth['auth_tag']=authTag
-                with open(_path,'wb') as _writ_r:
-                    pickle.dump(_d_cip_auth,_writ_r)
-                print(' {} . -----> Encrypted'.format(_path))
-    elif os.path.isfile(_file):  
-        print("\nIt is a file") 
-        with open(_file,'rb') as _read:
-            _data=_read.read()
-        _aesCipher = AES.new(secretKey, AES.MODE_GCM, nonce)
-        ciphertext, authTag = _aesCipher.encrypt_and_digest(_data)
-        _d_cip_auth={}
-        _d_cip_auth['cipher_text']=ciphertext
-        _d_cip_auth['auth_tag']=authTag
-        with open(_file,'wb') as _writ_r:
-            pickle.dump(_d_cip_auth,_writ_r)
-        print(' {} . -----> Encrypted'.format(_file))
-    else:  
-        print("It is a special file (socket, FIFO, device file)" )
-    
-_file='_g'
-_enc_d=encrypt_AES_GCM(_file)
-#print('file Encrypted')
-
-    #
-    
-#aesCipher = AES.new(secretKey, AES.MODE_GCM)
-   # _enc_data['aes_cipher']=aesCipher.nonce
-    #_enc_data['aes_Cipher']=aesCipher
-
-
-#ciphertext, authTag = aesCipher.encrypt_and_digest(msg)
-#    _enc_data['auth_Tag']=authTag
- #   with open(_file,'wb') as _bin_w:
-#        _bin_w.write(ciphertext)
-#    return _enc_data
+_path='de'
+encrypt_AES_GCM(_path)
